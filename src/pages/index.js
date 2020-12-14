b3944ef7d2d8d12f53a6941ac75a3dce6f95f292
Shane Biggs <moxdev1@gmail.com>
2020-12-14 13:14:06 -0500

    removed captcha

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,15 +14,6 @@ export default function Index() {
           <Link to="/contact/">Basic contact form</Link>
         </li>
         <li>
-          <Link to="/file-upload/">Form with file upload</Link>
-        </li>
-        <li>
-          <Link to="/multipart-file-upload/">Form with multipart file upload</Link>
-        </li>
-        <li>
-          <Link to="/recaptcha/">Form with reCAPTCHA 2</Link>
-        </li>
-        <li>
           You can also{' '}
           <a href="https://github.com/snipcart/gatsby-netlify-serverless/blob/master/functions/fetchreviews.js#L23">
             programmatically retrieve data
